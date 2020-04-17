4e576487c4dd661d31a478fb6f876a500fbaca2a
heidijane <heidi@heidisprouse.com>
2020-04-17 13:40:54 -0500

    Added a function that assesses if potter can be sold. If it can a price is given and it is pushed into a sellable pottery array

--- scripts/PotteryFactory.js
+++ scripts/PotteryFactory.js
@@ -1,12 +1,17 @@
 /*
     Pottery Factory
+    Functions that create a piece of pottery, fire it, and then determine if it can be sold
     
     Author: Heidi
 */
 
+//empty array to store pottery inventory
+const potteryToSell = []
+
+
 //generator function to create an ID
 function* autoIncrement() {
-    var index = 0
+    var index = 1
     while (true)
         yield index++
 }
@@ -36,3 +41,30 @@ export const FirePottery = (potteryObject, kilnTemp) => {
 
     return potteryObject
 }
+
+//determine if a piece of pottery can be sold and if so, give it a price and push it into our sellable pottery array
+export const assessPottery = (potteryObject, potteryToSellArray) => {
+    if (potteryObject.cracked === false) {
+        if (potteryObject.weight >= 3) {
+            potteryObject.price = 40
+        } else {
+            potteryObject.price = 20
+        }
+        potteryToSellArray.push(potteryObject)
+    }
+}
+
+//create 5 pieces of pottery
+const potteryArray = [
+    PotteryFactory("bowl", 2, .5),
+    PotteryFactory("bottle vase", 3, 3),
+    PotteryFactory("urn", 10, 2),
+    PotteryFactory("cylinder vase", 1, 2),
+    PotteryFactory("botanical jar", 5, 3)
+]
+
+potteryArray.forEach(pot => {
+    //create a random numer to simulate kiln temp fluctuations
+    const kilnTemp = Math.floor(Math.random() * (3000 - 1500)) + 1500;
+    assessPottery(FirePottery(pot, kilnTemp), potteryToSell)
+})
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,1 @@
-import { PotteryFactory, FirePottery } from "./PotteryFactory.js";
-
-console.log(FirePottery(PotteryFactory("round", 1, 2), 9000))
-console.log(FirePottery(PotteryFactory("square", 2, 3), 2200))
-console.log(FirePottery(PotteryFactory("long", 1, 1), 2100))
+import "./PotteryFactory.js";
